206a1f620c651a1c986ed5bb9b5c70b4c5ee5229
Jacklovestudy <877033946@qq.com>
2026-06-26 17:31:24 +0800

    update

---
+++ App.xaml.cs
@@ -0,0 +1,21 @@
+using Prism.DryIoc;
+using Prism.Ioc;
+using System.Windows;
+
+namespace ConvenientNote
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+using Prism.Mvvm;
+
+namespace ConvenientNote
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public string Title { get; } = "Convenient Note";
+    }
+}
